57a137ecb3b7c0e99b1fbb50a33d616bcb3c4e09
Val.istar.Guo <Val.istar.Guo@gmail.com>
2017-12-10 23:14:25 +0800

    更换server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,14 @@
 // PM2 Config
 const path = require('path');
+
+
 const { name: APP_NAME, repository: REPO } = require('./package.json');
+const serverPath = path.join('/var/www', APP_NAME);
+const user = 'www';
+const host = 'miaooo.me';
 
 module.exports = {
   apps: [
-    {
-      name: 'dev-app',
-      script: './build/server.dev.js',
-      watch: ['./server', './build', './framework'],
-      source_map_support: true,
-
-      env: {
-        NODE_ENV: 'develop',
-      },
-    },
     {
       name: `${APP_NAME}-stage`,
       script: './dist/server/bundle.js',
@@ -41,24 +36,24 @@ module.exports = {
 
   deploy: {
     prod: {
-      user: 'docker',
-      host: 'docker',
+      user,
+      host,
       ref: 'origin/master',
       repo: REPO,
-      path: path.join('/home/docker', APP_NAME, 'prod'),
-      'post-deploy': `yarn; npm run build:prod; pm2 startOrRestart ecosystem.config.js --only ${APP_NAME}-prod --env prod`,
+      path: path.join(serverPath, 'prod'),
+      'post-deploy': `npm i; npm run build:prod; pm2 startOrRestart ecosystem.config.js --only ${APP_NAME}-prod --env prod`,
 
       env: {
         NODE_ENV: 'prod',
       },
     },
     stage: {
-      user: 'docker',
-      host: 'docker',
+      user,
+      host,
       ref: 'origin/dev',
       repo: REPO,
-      path: path.join('/home/docker', APP_NAME, 'stage'),
-      'post-deploy': `yarn; npm run build:prod; pm2 startOrRestart ecosystem.config.js --only ${APP_NAME}-stage --env stage`,
+      path: path.join(serverPath, 'stage'),
+      'post-deploy': `npm i; npm run build:prod; pm2 startOrRestart ecosystem.config.js --only ${APP_NAME}-stage --env stage`,
     },
 
     env: {
