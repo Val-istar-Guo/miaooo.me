e84fc51017a3e3e99699f8a4c8ebffcd3301292a
Val.istar.Guo <val.istar.guo@gmail.com>
2018-11-23 00:39:57 +0800

    feat: support article code highlight

--- client/utils/highlightCode.js
+++ client/utils/highlightCode.js
@@ -1,12 +1,39 @@
 import visit from 'unist-util-visit'
-import { join } from 'path'
+import Prism from 'prismjs'
+import parse from 'rehype-parse'
+import unified from 'unified'
+import modifyChildren from 'unist-util-modify-children'
 
+
+const codeSelector = node => node.tagName === 'code'
+const preSelector = node => node.tagName === 'pre'
+
+const parseCode = lang => (node, index, parent) => {
+  if (node.type === 'text' && Prism.languages[lang]) {
+    const html = Prism.highlight(node.value, Prism.languages[lang], lang);
+    const tree = unified()
+      .use(parse, { fragment: true })
+      .parse(html)
+
+    parent.children.splice(index, 1, ...tree.children)
+    return index + tree.children.length;
+  }
+}
+
+const getLang = node => {
+  const lang = node.properties.className && node.properties.className[0] || ''
+  return lang.replace(/^language-/, '')
+}
+
+const codeVisitor = node => {
+  const lang = getLang(node)
+  modifyChildren(parseCode(lang))(node)
+}
+
+const preVisitor = node => {
+  visit(node, codeSelector, codeVisitor)
+}
 
 export default () => (tree, file) => {
-  const selector = node => node.tagName === 'pre'
-  const visitor = node => {
-    console.dir(node, { depth: null })
-  }
-
-  visit(tree, selector, visitor)
+  visit(tree, preSelector, preVisitor)
 }
