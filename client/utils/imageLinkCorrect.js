b6825290d3be5dc7088e76c492ba6231a5a61575
Val.istar.Guo <val.istar.guo@gmail.com>
2019-03-05 16:58:40 +0800

    fix: 图片无法显示
    github的图片链接出现变更导致的问题

--- client/utils/imageLinkCorrect.js
+++ client/utils/imageLinkCorrect.js
@@ -5,7 +5,8 @@ import { join } from 'path'
 export default () => (tree, file) => {
   const selector = node => node.type === 'image' && node.url[0] === '.'
   const visitor = node => {
-    node.url = `${join('https://github.com/Val-istar-Guo/article/raw/master/', '/articles', node.url)}?sanitize=true`
+
+    node.url = `${join('https://raw.githubusercontent.com/Val-istar-Guo/article/master', '/articles', node.url)}?sanitize=true`
   }
 
   visit(tree, selector, visitor)
